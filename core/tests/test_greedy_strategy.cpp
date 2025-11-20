fb248c3b49de99824bd5f78f5249e7be66dd81b8
Quaha <ArtemRomanov2017@yandex.ru>
2025-11-20 17:03:56 +0300

    Greedy Strategy

--- core/tests/test_greedy_strategy.cpp
+++ core/tests/test_greedy_strategy.cpp
@@ -9,3 +9,53 @@
 TEST(GreedyStrategy, CanUseStrategy) {
     EXPECT_NO_THROW(GreedyStrategy(Matrix(5)));
 }
+
+TEST(GreedyStrategy, StrategyCorrectnessM0x0) {
+    Matrix m(0);
+
+    std::vector<int> expected;
+
+    EXPECT_EQ(GreedyStrategy(m), expected);
+}
+
+TEST(GreedyStrategy, StrategyCorrectnessM1x1) {
+    Matrix m(1);
+    m(0, 0) = 1.0;
+
+    std::vector<int> expected = { 0 };
+
+    EXPECT_EQ(GreedyStrategy(m), expected);
+}
+
+TEST(GreedyStrategy, StrategyCorrectnessM2x2) {
+    Matrix m(2);
+    m(0, 0) = 4.0; m(0, 1) = 2.0;
+    m(1, 0) = 2.0; m(1, 1) = 1.0;
+
+    std::vector<int> expected = { 0, 1};
+
+    EXPECT_EQ(GreedyStrategy(m), expected );
+}
+
+TEST(GreedyStrategy, StrategyCorrectnessM3x3) {
+    Matrix m(3);
+    m(0, 0) = 1.0; m(0, 1) = 2.0; m(0, 2) = 4.0;
+    m(1, 0) = 2.0; m(1, 1) = 3.0; m(1, 2) = 4.0;
+    m(2, 0) = 1.0; m(2, 1) = 1.0; m(2, 2) = 3.0;
+
+    std::vector<int> expected = { 1, 0, 2 };
+
+    EXPECT_EQ(GreedyStrategy(m), expected);
+}
+
+TEST(GreedyStrategy, StrategyCorrectnessM4x4) {
+    Matrix m(4);
+    m(0, 0) = 1.0; m(0, 1) = 2.0; m(0, 2) = 9.0; m(0, 3) = 1.0;
+    m(1, 0) = 2.0; m(1, 1) = 1.0; m(1, 2) = 4.0; m(1, 3) = 3.0;
+    m(2, 0) = 6.0; m(2, 1) = 7.0; m(2, 2) = 2.0; m(2, 3) = 9.0;
+    m(3, 0) = 3.0; m(3, 1) = 1.5; m(3, 2) = 3.0; m(3, 3) = 4.0;
+
+    std::vector<int> expected = { 2, 0, 1, 3 };
+
+    EXPECT_EQ(GreedyStrategy(m), expected);
+}
--- core/tests/test_matrix.cpp
+++ core/tests/test_matrix.cpp
@@ -12,38 +12,42 @@ TEST(Matrix, CantCreateIncorrectMatrix) {
 }
 
 TEST(Matrix, CanAccessElement) {
-    Matrix matrix(3);
+    Matrix m(3);
 
-    EXPECT_NO_THROW(matrix(1, 1));
+    EXPECT_NO_THROW(m(1, 1));
 }
 
 TEST(Matrix, CantAccessElementWithIncorrectIndex) {
-    Matrix matrix(3);
+    Matrix m(3);
 
-    EXPECT_ANY_THROW(matrix(3, 1));
-    EXPECT_ANY_THROW(matrix(1, 3));
-    EXPECT_ANY_THROW(matrix(-1, 0));
-    EXPECT_ANY_THROW(matrix(0, -1));
+    EXPECT_ANY_THROW(m(3, 1));
+    EXPECT_ANY_THROW(m(1, 3));
+    EXPECT_ANY_THROW(m(-1, 0));
+    EXPECT_ANY_THROW(m(0, -1));
 }
 
 TEST(Matrix, CanModifyElement) {
-    Matrix matrix(2);
+    Matrix m(2);
 
-    EXPECT_NO_THROW(matrix(0, 0) = 5.0);
-    EXPECT_NEAR(matrix(0, 0), 5.0, EPS);
+    EXPECT_NO_THROW(m(0, 0) = 5.0);
+    EXPECT_NEAR(m(0, 0), 5.0, EPS);
 }
 
 TEST(Matrix, CantModifyElementWithIncorrectIndex) {
-    Matrix matrix(2);
+    Matrix m(2);
 
-    EXPECT_ANY_THROW(matrix(2, 0) = 3.0);
-    EXPECT_ANY_THROW(matrix(0, 2) = 3.0);
-    EXPECT_ANY_THROW(matrix(-1, 0) = 3.0);
-    EXPECT_ANY_THROW(matrix(0, -1) = 3.0);
+    EXPECT_ANY_THROW(m(2, 0) = 3.0);
+    EXPECT_ANY_THROW(m(0, 2) = 3.0);
+    EXPECT_ANY_THROW(m(-1, 0) = 3.0);
+    EXPECT_ANY_THROW(m(0, -1) = 3.0);
 }
 
 TEST(Matrix, CanGetSize) {
     Matrix m(3);
 
     EXPECT_EQ(m.size(), 3);
+}
+
+TEST(Matrix, CanFillMatrix) {
+    EXPECT_EQ(0, 1);
 }
