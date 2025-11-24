4858e68f089c45f7f77c2e6ee236890eec10cb49
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2025-11-25 00:42:02 +0300

    integrated json into ProcessJSON

--- core/src/process_json.cpp
+++ core/src/process_json.cpp
@@ -2,7 +2,9 @@
 
 #include <iostream>
 #include <algorithm>
+#include <nlohmann/json.hpp>
 
+#include "json_schemas.hpp"
 #include "matrix.hpp"
 #include "greedy_strategy.hpp"
 #include "thrifty_strategy.hpp"
@@ -11,25 +13,18 @@
 #include "median_strategy.hpp"
 #include "optimal_strategy.hpp"
 
+using json = nlohmann::json;
+
+static std::string json_result;
+
 DLL_API const char* ProcessJSON(const char* json_input) {
 
-    int T = 50;
-    int n = 15;
-
-    double alpha_min = 0.12;
-    double alpha_max = 0.22;
-
-    double beta_1 = 0.86;
-    double beta_2 = 0.99;
-
-    bool concentrated = true;
-    bool maturation = true;
-    bool inorganic = true;
-
-    int v = n / 2;
-    double beta_max = 1.1;
-
-    // CODE WITH JSON PROCESSING WOULD BE HERE (INPUT)
+    auto parsing_result = SimulationParams::from_json(json_input);
+    if (std::holds_alternative<ParsingError>(parsing_result))
+    {
+        return std::get<ParsingError>(parsing_result).message.c_str();
+    }
+    auto params = std::get<SimulationParams>(parsing_result);
 
     std::vector<std::string> names = {
         "greedy",
@@ -43,16 +38,23 @@ DLL_API const char* ProcessJSON(const char* json_input) {
     int S = names.size();
 
     // avg_step_ratio[s][i] Ч средний % выбранного значени€ от максимума на шаге i
-    std::vector<std::vector<double>> avg_step_ratio(S, std::vector<double>(n, 0.0));
+    std::vector<std::vector<double>> avg_step_ratio(S, std::vector<double>(params.getN(), 0.0));
 
     // avg_loss[s] Ч средн€€ потер€ относительно optimal
     std::vector<double> avg_loss(S, 0.0);
 
-    Matrix m(n);
+    Matrix m(params.getN());
 
-    for (int test_case = 0; test_case < T; ++test_case) {
+    for (int test_case = 0; test_case < params.getT(); ++test_case) {
 
-        m.fillMatrix(alpha_min, alpha_max, beta_1, beta_2, concentrated, maturation, inorganic, v, beta_max);
+        if (params.hasMaturation()) {
+            m.fillMatrix(params.getAlphaMin(), params.getAlphaMax(), params.getBeta1(), params.getBeta2(),
+                params.isConcentrated(), params.hasMaturation(), params.isInorganic(), 
+                params.getMaturationParams().value().getV(), params.getMaturationParams().value().getBetaMax());
+        } else {
+            m.fillMatrix(params.getAlphaMin(), params.getAlphaMax(), params.getBeta1(), params.getBeta2(), 
+                params.isConcentrated(), params.hasMaturation(), params.isInorganic(), 1, 1.0);
+        }
 
         std::vector<std::vector<int>> seqs;
 
@@ -68,7 +70,7 @@ DLL_API const char* ProcessJSON(const char* json_input) {
         for (int s = 0; s < S; ++s) {
             const auto& seq = seqs[s];
 
-            for (int i = 0; i < n; ++i) {
+            for (int i = 0; i < params.getN(); ++i) {
                 double val = m(seq[i], i);
                 avg_step_ratio[s][i] += val;
             }
@@ -79,13 +81,13 @@ DLL_API const char* ProcessJSON(const char* json_input) {
     }
 
     for (int s = 0; s < S; ++s) {
-        for (int i = 0; i < n; ++i) {
-            avg_step_ratio[s][i] /= T;
+        for (int i = 0; i < params.getN(); ++i) {
+            avg_step_ratio[s][i] /= params.getT();
         }
-        avg_loss[s] /= T;
+        avg_loss[s] /= params.getT();
     }
 
-    // CODE WITH JSON PROCESSING WOULD BE HERE (OUTPUT)
+    json_result = Solution::to_json(names, avg_step_ratio, avg_loss);
 
-    return json_input;
+    return json_result.c_str();
 }
